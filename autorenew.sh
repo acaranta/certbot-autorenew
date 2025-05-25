18fa6c385c3a95c91e2ca886e949ef9aa3d2bc85
acaranta <arthur@caranta.com>
2025-05-25 23:54:47 +0200

    Refactor autorenew script to properly encapsulate CRTDIR copy operations within a conditional block

--- autorenew.sh
+++ autorenew.sh
@@ -9,8 +9,11 @@ do
 		do 
 			cat /etc/letsencrypt/live/${cert}/privkey.pem >/tmp/certbot_${cert}.crt
 			cat /etc/letsencrypt/live/${cert}/fullchain.pem >> /tmp/certbot_${cert}.crt
-			cp ${CRTDIR}/certbot_${cert}.crt ${CRTDIR}/certbot_${cert}.crt.old
-			cp /tmp/certbot_${cert}.crt ${CRTDIR}
+			if [ -n "${CRTDIR}" ]; then
+				echo "Copying certbot_${cert}.crt to ${CRTDIR}"
+				cp ${CRTDIR}/certbot_${cert}.crt ${CRTDIR}/certbot_${cert}.crt.old
+				cp /tmp/certbot_${cert}.crt ${CRTDIR}
+			fi
 			if [ -n "${MINIO_URL}" ]; then
 				echo "Uploading certbot_${cert}.crt to MinIO S3 at ${MINIO_BUCKET}/${MINIO_PATH}"
 				/upload-to-minio.sh ${MINIO_URL} ${MINIO_USER} ${MINIO_PASS} ${MINIO_BUCKET} ${MINIO_PATH}/certbot_${cert}.crt /tmp/certbot_${cert}.crt
